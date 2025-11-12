5e7927d7c6c068ce9a2ff89c886fa22e9d6e2415
Luana Araújo <aninhaluua@gmail.com>
2025-11-12 10:10:35 -0300

    Add files via upload

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "olá, mundo!";
+const texto2 = 'olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "meu nome é ";
+const meuNome = "Luana";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
